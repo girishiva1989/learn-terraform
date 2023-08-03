b20380949694f23099176bac3309a7fc5df8d7b8
Girish <girishiva1989@gmail.com>
2023-08-03 19:36:01 +0530

    variables

--- 01-Vars/output.tf
+++ 01-Vars/output.tf
@@ -1,11 +1,12 @@
 #Print variable
+#In Terraform, how we give variable name while declaring it, in the similar manner we give name while printing
 
-output "sample_string" {
+output "sample1" {
   value = var.sample_string
 }
 
 #If printhing variable with additional string
 
-output "sample_string1" {
+output "sample2" {
   value = "value of sample string = ${var.sample_string}"
 }
