13a984e0e22636572a4d3d80b9c5ddfb774d7315
girish <girishiva1989@gmail.com>
2024-07-22 09:28:23 +1000

    Add function

--- 03-loops/for_each/main.tf
+++ 03-loops/for_each/main.tf
@@ -1,21 +1,21 @@
-# variable "fruits_map" {
-#   default = {
-#     apple   = 10
-#     banana  = 100
-#     oranges = 200
-#   }
-# }
-#
-# resource "null_resource" "fruits_nane" {
-#
-#   for_each = var.fruits_map
-#
-#   provisioner "local-exec" {
-#     command = "echo Fruit Name - ${each.key} - ${each.value}"
-#     //command = "echo  ${length(var.fruits)}"
-#   }
-#
-# }
+variable "fruits_map" {
+  default = {
+    apple   = 10
+    banana  = 100
+    oranges = 200
+  }
+}
+
+resource "null_resource" "fruits_nane" {
+
+  for_each = var.fruits_map
+
+  provisioner "local-exec" {
+    command = "echo Fruit Name - ${each.key} - ${each.value}"
+    //command = "echo  ${length(var.fruits)}"
+  }
+
+}
 
 variable "fruits_multi_map" {
   default = {
@@ -39,24 +39,24 @@ resource "null_resource" "fruits1" {
   for_each = var.fruits_multi_map
 
   provisioner "local-exec" {
-#     command = "echo Fruit Name - ${each.value["name"]} - ${each.value["count"]}"
-    command = "echo Fruit Name - ${each.key} - ${each.value["name"]}"
+      command = "echo Fruit Name - ${each.value["name"]} - ${each.value["count"]}"
+    //command = "echo Fruit Name - ${each.key} - ${each.value["name"]}"
     //command = "echo  ${length(var.fruits)}"
   }
 
 }
-#
-# // For-each on a list
-# variable "vegetables" {
-#   default = ["carrot ", "capsicum"]
-# }
-#
-# resource "null_resource" "vegetables" {
-#
-#   for_each = toset(var.vegetables)
-#
-#   provisioner "local-exec" {
-#     command = "echo Fruit Name - ${each.key}"
-#   }
-#
-#}
+
+// For-each on a list
+variable "vegetables" {
+  default = ["carrot ", "capsicum"]
+}
+
+resource "null_resource" "vegetables" {
+
+  for_each = toset(var.vegetables)
+
+  provisioner "local-exec" {
+    command = "echo Fruit Name - ${each.key}"
+  }
+
+}
