de94e9561bcc5b1d550d3799c0344955db594691
girish <girishiva1989@gmail.com>
2024-07-30 12:33:12 +1000

    Add function

---
+++ ex2/sg/main.tf
@@ -0,0 +1,25 @@
+resource "aws_security_group" "example" {
+  name        = "example-sg"
+  description = "Example security group"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+output "sg_id" {
+  value = aws_security_group.example.id
+}
