a058839578c7be8f65d9c01961a747855f580798
girish <girishiva1989@gmail.com>
2024-07-30 12:40:14 +1000

    Add function

--- ex2/ec2/main.tf
+++ ex2/ec2/main.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "instance" {
-  ami           = "ami-03265a0778a880afb"
+  ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = [var.sg_id]
 
